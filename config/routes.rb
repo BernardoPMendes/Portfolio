b1c6664df9dfe0fecf8d2df166102540b4ebd985
bernardo mendes <bmmendes2001@icloud.com>
2023-03-22 20:29:37 +0000

    all done for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  root 'portfolio#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :portfolio, only: [:index, :show]
 end
